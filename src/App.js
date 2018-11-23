98bf1de8869a3ffb1871903a989aea1d16f9e314
Zeli <ak47m40080@gmail.com>
2018-11-23 18:34:37 +1100

    2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {languages:['Javascript','Java','Python','Ruby','PHP','C++','CSS','C#','C','Go','Shell','Objective C','Scala','Swift','Typescript']}
+  state = {languages:[
+    {id:1,val:'Javascript'},
+    {id:2,val:'Java'},
+    {id:3,val:'Python'},
+    {id:4,val:'Ruby'},
+    {id:5,val:'PHP'},
+    {id:6,val:'C++'},
+    {id:7,val:'CSS'},
+    {id:8,val:'C#'},
+    {id:9,val:'C'},
+    {id:10,val:'Go'},
+    {id:11,val:'Shell'},
+    {id:12,val:'Objective C'},
+    {id:13,val:'Scala'},
+    {id:14,val:'Swift'},
+    {id:15,val:'Typescript'}
+  ]
+}
 
   render() {
     const lanEles = this.state.languages.map((lan,index) => (
-      <tr>
+      <tr key={lan.id}>
+        <td>{lan.id}</td>
+        <td>{lan.val}</td>
         <td>{index + 1}</td>
-        <td>{lan}</td>
         <td><button onClick = {()=>{
           let lans = [...this.state.languages];
           lans.splice(index,1);
@@ -21,8 +39,9 @@ class App extends Component {
       <div className="App">
         <table>
           <tr>
+            <th>id</th>
             <th>Name</th>
-            <th>Rank</th>
+            <th>Index</th>
           </tr>
           {lanEles}
         </table>
